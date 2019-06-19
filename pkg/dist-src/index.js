8525b1dae4e834d8001d3e23ae781906f32c9d1a
vitaliytv <vitaliytv@nitralabs.com>
2019-06-19 14:35:22 +0300

    log level

--- pkg/dist-src/index.js
+++ pkg/dist-src/index.js
@@ -11,13 +11,37 @@ const checkEnv = require('@47ng/check-env').default;
 checkEnv({
   required: ['X_NITRA_CF_KEY']
 });
+
+const log = require('loglevel-colored-level-prefix')();
+
+log.debug('cfSecurity in DEBUG MODE');
 /**
  * Represents a book.
  *
- * @param {string} cfKey - The value for check
+ * @param {object} req - The value for check
  * @return {boolean} if value equal env
  */
 
-exports.cfSecurity = function (cfKey) {
-  return cfKey.length > 0 && cfKey === process.env['X_NITRA_CF_KEY'];
+exports.cfSecurity = function (req) {
+  if (typeof req.headers !== 'undefined') {
+    log.debug(`Header not exist in request`);
+    return false;
+  }
+
+  if (typeof req.headers['x_nitra_cf_key'] !== 'undefined') {
+    log.debug(`Nitra key not exist in headers request`);
+    return false;
+  }
+
+  if (req.headers['x_nitra_cf_key'].length === 0) {
+    log.debug(`Empty Nitra key in headers request`);
+    return false;
+  }
+
+  if (req.headers['x_nitra_cf_key'] !== process.env['X_NITRA_CF_KEY']) {
+    log.debug(`Not equal Nitra key`);
+    return false;
+  }
+
+  return true;
 };
