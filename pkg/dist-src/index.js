18d9e18feb184f296f21e8059bc4cd4ddbc46291
vitaliytv <vitaliytv@nitralabs.com>
2019-06-20 08:32:45 +0300

    1.0.3

--- pkg/dist-src/index.js
+++ pkg/dist-src/index.js
@@ -1,11 +1,11 @@
 /**
- * CF Securoty module
+ * CF Security module
  * @module @nitra/cf-security
  */
 
 /**
-* @const {Function}
-*/
+ * @const {Function}
+ */
 const checkEnv = require('@47ng/check-env').default;
 
 checkEnv({
@@ -16,29 +16,33 @@ const log = require('loglevel-colored-level-prefix')();
 
 log.debug('cfSecurity in DEBUG MODE');
 /**
- * Represents a book.
- *
- * @param {object} req - The value for check
- * @return {boolean} if value equal env
- */
+* Check request for Nitra security rules
+*
+* @param {object} req - ApolloServer or Express Request for check
+* @return {boolean} if check passed
+*/
 
 exports.cfSecurity = function (req) {
+  let cfKey; // If this ApolloServer reqest
+
   if (typeof req.headers !== 'undefined') {
-    log.debug(`Header not exist in request`);
-    return false;
+    if (typeof req.headers['x_nitra_cf_key'] === 'undefined') {
+      log.debug(`Nitra key not exist in ApolloServer request`);
+      return false;
+    }
+
+    cfKey = req.headers['x_nitra_cf_key'];
+  } else {
+    // Express request
+    cfKey = req.get('x_nitra_cf_key');
   }
 
-  if (typeof req.headers['x_nitra_cf_key'] !== 'undefined') {
-    log.debug(`Nitra key not exist in headers request`);
-    return false;
-  }
-
-  if (req.headers['x_nitra_cf_key'].length === 0) {
+  if (cfKey.length === 0) {
     log.debug(`Empty Nitra key in headers request`);
     return false;
   }
 
-  if (req.headers['x_nitra_cf_key'] !== process.env['X_NITRA_CF_KEY']) {
+  if (cfKey !== process.env['X_NITRA_CF_KEY']) {
     log.debug(`Not equal Nitra key`);
     return false;
   }
