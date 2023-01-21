48a5a6337cd8d62fdd110ffb6db632885130aa94
elenatv <elena@nitralabs.com>
2023-01-21 22:54:20 +0200

    "main": "src/index.js"

--- npm/src/index.js
+++ npm/src/index.js
@@ -1,4 +1,3 @@
-import getLogger from '@nitra/bunyan/trace'
 import checkEnv from '@nitra/check-env'
 checkEnv(['X_NITRA_CF_KEY'])
 
@@ -9,25 +8,23 @@ checkEnv(['X_NITRA_CF_KEY'])
  * @return {boolean} if check passed
  */
 export const cfSecurity = req => {
-  const log = getLogger(req)
-
   if (typeof req.headers === 'undefined') {
-    log.info('Request without headers')
+    req.log.info('Request without headers')
     return false
   }
 
   if (typeof req.headers['x-nitra-cf-key'] === 'undefined') {
-    log.info('Nitra key not exist in request')
+    req.log.info('Nitra key not exist in request')
     return false
   }
 
   if (req.headers['x-nitra-cf-key'] === 0) {
-    log.info('Empty Nitra key in headers request')
+    req.log.info('Empty Nitra key in headers request')
     return false
   }
 
   if (req.headers['x-nitra-cf-key'] !== process.env.X_NITRA_CF_KEY) {
-    log.info('Not equal Nitra key')
+    req.log.info('Not equal Nitra key')
     return false
   }
 
